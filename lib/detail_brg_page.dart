9d2fabfb5b243181c5764352edc88da63be798a2
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-04-29 22:51:09 +0700

    Menambahkan parameter class DetailBrgpage.

--- lib/detail_brg_page.dart
+++ lib/detail_brg_page.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
 class DetailBrgPage extends StatelessWidget {
-  const DetailBrgPage({super.key});
+  final String email;
+  final String jumlahBarang;
+  final String tanggal;
+  final String jenisTransaksi;
+  final String jenisBarang;
+  final String hargaSatuan;
+  final int totalHarga;
 
+  const DetailBrgPage({
+    super.key,
+    required this.jumlahBarang,
+    required this.tanggal,
+    required this.jenisTransaksi,
+    required this.jenisBarang,
+    required this.hargaSatuan,
+    required this.totalHarga,
+    required this.email,
+  });
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
