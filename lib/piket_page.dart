cfd21e948d88bafd6da400095b717a74fc40423b
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-04-29 20:34:41 +0700

    Inisialisasi controller inputan dan form key untuk  PiketPage.

--- lib/piket_page.dart
+++ lib/piket_page.dart
@@ -3,14 +3,21 @@ import 'package:flutter/material.dart';
 class PiketPage extends StatefulWidget {
   final String email;
   const PiketPage({super.key, required this.email});
-  
+
   @override
   State<PiketPage> createState() => _PiketPageState();
 }
 
 class _PiketPageState extends State<PiketPage> {
+  late TextEditingController namaController = TextEditingController(
+    text: widget.email,
+  );
+  final TextEditingController tanggalController = TextEditingController();
+  final TextEditingController tugasController = TextEditingController();
+  final piketKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
