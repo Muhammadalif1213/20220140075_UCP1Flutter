bf015c4175ebb6cd67413776782b05b2ed483f39
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-04-29 20:16:17 +0700

    Menambahkan stac yang berisi container, text, image, dan button.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -68,6 +68,62 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.blue, 
+                          const Color.fromARGB(255, 12, 24, 185),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 1,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/images/mini.png' , width: 200),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Dapatkan Diskon \n25% disetiap \nharinya',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          ElevatedButton(onPressed: (){}, child: Text('Order Sekarang', style: TextStyle(fontSize: 12),))
+                        ],
+                      ),
+                      
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
