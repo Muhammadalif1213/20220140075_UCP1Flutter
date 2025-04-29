efc14fc96043fcd65c57e618ee64e2c9969f9a3d
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-04-29 22:10:32 +0700

    Membuat  BrgPage sebagai StatefulWidget dengan parameter email.

---
+++ lib/brg_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BrgPage extends StatefulWidget {
+  final String email;
+  const BrgPage({super.key, required this.email});
+
+  @override
+  State<BrgPage> createState() => _BrgPageState();
+}
+
+class _BrgPageState extends State<BrgPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
