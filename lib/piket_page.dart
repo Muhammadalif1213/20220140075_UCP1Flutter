11fa3057e45d69ad2f5f4a1beb84d1d739d68052
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-04-29 20:42:49 +0700

    Menambahkan AppBar dengan tombol kembali dan judul di PiketPage.

--- lib/piket_page.dart
+++ lib/piket_page.dart
@@ -57,6 +57,42 @@ class _PiketPageState extends State<PiketPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80.0),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.blueAccent,
+          elevation: 0,
+          flexibleSpace: SafeArea(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 25, right: 20, left: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back, color: Colors.white),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    Text(
+                      'Piket Gudang',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(width: 50),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
