03017cd75c02fba5c44d4ac821463c5a0c6f757b
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-04-29 22:49:01 +0700

    Menambahkan statelesWidget DetailBrgPage.

---
+++ lib/detail_brg_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailBrgPage extends StatelessWidget {
+  const DetailBrgPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
